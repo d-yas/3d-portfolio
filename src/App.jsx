7c5fe663176ed0fd5887551fe0443b97c5bad64d
d-yas <denizcanyas@gmail.com>
2024-09-26 22:04:10 +0300

    env setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import './index.css'
+import Header from "./components/Header";
+/* import Hero from "./components/Hero";
+import Sea from "./components/Sea";
+import Id from "./components/Id";
+import Swiss from "./components/Swiss";
+import Laptop from "./components/Laptop";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+ */
 
-export default function App() {
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        {/* <Hero />
+        <Sea />
+        <Id />
+        <Swiss />
+        <Laptop />
+        <Projects />
+        <Footer /> */}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:static lg:flex lg:mx-auto lg:backdrop-blur-sm`}>
+        <div className="font-code">
+            Denizcan.
+        </div>
+        </div>
+  )
+}
+
+export default Header
