97e58a2e21126543af076a75973a2ff9be644c6d
d <denizcanyas@gmail.com>
2024-09-29 15:06:38 +0300

    glsl ekledim

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import RagingSea from "./components/RagingSea";
+import { Canvas } from '@react-three/fiber'
+
 
 
 /* import Id from "./components/Id";
@@ -15,7 +17,17 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <RagingSea />
+        <Canvas className="fixed top-0 left-0 w-full h-full bg-w"
+          camera={ {
+              fov: 45,
+              near: 0.1,
+              far: 200,
+              position: [ 4, - 2, 6 ]
+          } }
+      >
+          <RagingSea />
+      </Canvas>
+        
       </div>
     </>
   );
--- src/components/RagingSea.jsx
+++ src/components/RagingSea.jsx
@@ -1,15 +1,47 @@
-import { shaderMaterial } from '@react-three/drei'
-import {useRef} from 'react'
+import { OrbitControls, shaderMaterial } from '@react-three/drei'
+import * as THREE from 'three';
+import waterVertex from '../shaders/water/vertex.glsl';
+import waterFragment from '../shaders/water/fragment.glsl';
+import { useRef } from 'react';
+import { useFrame,extend } from '@react-three/fiber';
 
-const RagingSea = () => {
-  return (
-    <mesh>
-        <planeGeometry/>
-        <primitive object={shaderMaterial} attach="material" ref={materialRef}/>
+const WaterMaterial = shaderMaterial(
+    {
+        uTime: 0,
         
-    </mesh>
-  )
-}
+        uBigWavesElevation: 0.2,
+        uBigWavesFrequency: new THREE.Vector2(4, 1.5) ,
+        uBigWavesSpeed: 0.75,
 
-import Hero from "./components/Hero";
-export default RagingSea
+        uSmallWavesElevation: 0.15,
+        uSmallWavesFrequency: 3,
+        uSmallWavesSpeed: 0.2,
+        uSmallIterations: 4,
+
+        uDepthColor: { value: new THREE.Color("#3A8EBA") },
+        uSurfaceColor: { value: new THREE.Color("#3A8EBA") },
+        uColorOffset: 0.925,
+        uColorMultiplier: 1
+    },
+    waterVertex,
+    waterFragment
+)
+extend({WaterMaterial})
+
+export default function RagingSea()
+{
+    const waterMaterial = useRef()
+    useFrame((state,delta) => {     
+        waterMaterial.current.uTime += delta
+    })
+    return <>
+        
+        <OrbitControls makeDefault />
+        <ambientLight intensity={ 1.5 } />  
+        <mesh rotation-x={ - Math.PI * 0.5 }scale={1.5} >
+            <planeGeometry args={[2, 2, 128, 128]} /> 
+            <waterMaterial ref={waterMaterial} />
+        </mesh>
+
+    </>
+}
