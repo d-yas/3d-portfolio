adbb0c61528cdd3cf2f97b4b214bc4489bfb3142
d-yas <denizcanyas@gmail.com>
2024-10-19 16:29:21 +0300

    de

--- src/components/IntroPhone.jsx
+++ src/components/IntroPhone.jsx
@@ -1,29 +1,29 @@
 import { Html } from "@react-three/drei";
-import ProfilePhone from "./ProfilePhone";
-import WebPhone from "./WebPhone";
-import DataPhone from "./DataPhone";
-import FooterPhone from "./FooterPhone";
-import HeaderPhone from "./HeaderPhone";
-import AppsPhone from "./AppsPhone";
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 
 const IntroPhone = () => {
-  const [selectedApp, setSelectedApp] = useState(null);
- 
+  const [isIPhone, setIsIPhone] = useState(false);
+
+  useEffect(() => {
+    // Check if the user agent matches an iPhone
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/iPhone/i.test(userAgent)) {
+      setIsIPhone(true);
+    }
+  }, []);
 
   return (
     <Html
       transform
       distanceFactor={1.28}
-      position={[0.165,1.34,0.08]}
+      position={isIPhone ? [0, 1.34, 0.08] : [0.165, 1.34, 0.08]}
     >
- 
       <iframe
-    src="https://3d-portfolio-mobile.vercel.app/"
-    width="465px"
-    height="1010px"
-    className={`rounded-[4rem]`}
-  />
+        src="https://3d-portfolio-mobile.vercel.app/"
+        width="465px"
+        height="1010px"
+        className="rounded-[4rem]"
+      />
     </Html>
   );
 };
