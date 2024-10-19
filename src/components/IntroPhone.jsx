09be5972ec7d4c39c6d81bdc970b044babd5f324
d-yas <denizcanyas@gmail.com>
2024-10-20 02:35:47 +0300

    denem

--- src/components/IntroPhone.jsx
+++ src/components/IntroPhone.jsx
@@ -16,15 +16,17 @@ const IntroPhone = () => {
     <Html
       transform
       distanceFactor={1.28}
-      position={isIPhone ? [-0.525, 1.34, 0.08] : [0.165, 1.34, 0.08]}
+      position={[-0.57, 2.96, 0.08]}
+      /* position={isIPhone ? [-0.525, 1.34, 0.08] : [0.165, 1.34, 0.08]} */
       pointerEvents="auto"
-      center
+      
     >
       <iframe
         src="https://3d-portfolio-mobile.vercel.app/"
         width="465px"
         height="1010px"
-        className="rounded-[4rem] pointer-events-auto z-[9494]"
+        className="rounded-[4rem] absolute pointer-events-auto z-[9494]"
+        
       />
     </Html>
   );
